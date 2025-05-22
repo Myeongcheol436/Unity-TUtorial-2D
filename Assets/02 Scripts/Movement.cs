fd86c186611d1c15bf53b0e1d1acaf5ba60be677
Myeongcheol436 <zkdl436@naver.com>
2025-05-22 17:25:34 +0900

    cube,movement
    ---큐브 만들기
    ---큐브 이름 바꾸기
    ---움직이기

--- Assets/02 Scripts/Movement.cs
+++ Assets/02 Scripts/Movement.cs
@@ -18,23 +18,30 @@ public class Movement : MonoBehaviour
     {
         //this.transform.position = this.transform.position + Vector3.forward * MoveSpeed;
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += Vector3.forward * MoveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {   
-            transform.position += Vector3.left * MoveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position += Vector3.back * MoveSpeed * Time.deltaTime;
-     
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += Vector3.right * MoveSpeed * Time.deltaTime; 
-        }
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    transform.position += Vector3.forward * MoveSpeed * Time.deltaTime;
+        //}
+        //if (Input.GetKey(KeyCode.A))
+        //{   
+        //    transform.position += Vector3.left * MoveSpeed * Time.deltaTime;
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.position += Vector3.back * MoveSpeed * Time.deltaTime;
+        //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    transform.position += Vector3.right * MoveSpeed * Time.deltaTime; 
+        //}
 
+        float h = Input.GetAxis("Horizontal"); //가로세로
+        float v = Input.GetAxis("Vertical"); //수직 위 아래
+
+        Vector3 dir = new Vector3(h, 0, v);
+        Debug.Log($"현재 입력 : {dir}");
+
+        transform.position += dir * MoveSpeed * Time.deltaTime;
         }
     }
-}
+
--- Assets/02 Scripts/StudyComponent.cs
+++ Assets/02 Scripts/StudyComponent.cs
@@ -3,15 +3,65 @@ using UnityEngine.UIElements;
 
 public class StudyComponent : MonoBehaviour
 {
-    GameObject obj;
+    public GameObject obj;
 
-    public string change_Name;
+    public Mesh msh;
+    public Material mat;
+
+    //public Transform objTf;
+
+    //public string change_Name;
 
     void Start()
     {
+        obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        CreateCube("Hello World");
+        CreateCube();
+    }
 
-        obj = GameObject.Find("Main Camera");
-        obj.name = change_Name;
-        //obj.Transform.Position = 5;
+    public void CreateCube(string name = "Cube")
+    {
+        obj = new GameObject(name);
+
+        obj.AddComponent<MeshFilter>();
+        obj.AddComponent<MeshRenderer>();
+        obj.AddComponent<BoxCollider>();
+        //string[] monster = { "슬라임, 사막뱀, 악마" };
+        //int[] monster_level = new int[4];
+        //monster_level[0] = 1;
+        //monster_level[1] = 6;
+        //monster_level[2] = 20;
+
+
+        //obj = GameObject.FindGameObjectWithTag("Player");
+
+        //objTf = GameObject.FindGameObjectWithTag("Player").transform;
+
+        //Debug.Log($"<Color=#ff0000>이름 : {obj.name}</Color>");
+
+        //Debug.Log($"태그 :{obj.tag}");
+        //Debug.Log($"위치 : {obj.transform.position}");
+        //Debug.Log($"회전 : {obj.transform.rotation}");
+        //Debug.Log($"크기 : {obj.transform.localScale}");
+
+        //Debug.Log($"Mesh 데이터 : {obj.GetComponent<MeshFilter>().mesh}");
+
+        //Debug.Log($"Material 데이터 : {obj.GetComponent<MeshRenderer>().material}");
+
+        //obj.GetComponent<MeshRenderer>().enabled = false;
+
+        //obj.SetActive(false);
+        //objTf.gameObject.SetActive(false);
+
+        obj = new GameObject("Cube");
+
+        obj.AddComponent<MeshFilter>();
+        obj.GetComponent<MeshFilter>().mesh = msh;
+
+        obj.AddComponent<MeshRenderer>();
+        obj.GetComponent<MeshRenderer>().material = mat;
+
+        obj.AddComponent<BoxCollider>();
+        obj.GetComponent<MeshFilter>();
     }
 }
